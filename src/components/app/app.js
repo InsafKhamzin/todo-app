21561f24aec2b43e6ea7f82cec1597c507509ff3
InsafKhamzin <hamzin730insaf@gmail.com>
2019-06-09 19:38:14 +0300

    react todo app init

---
+++ src/components/app/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import TodoList from '../todo-list';
+import SearchPanel from '../search-panel';
+
+const App = () => {
+
+    const todoData = [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 },
+    ];
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList todos={todoData} />
+        </div>
+    );
+};
+
+export default App
